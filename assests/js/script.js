cfaa0755bcbb4fc61e5816d434530234bbcac20f
chris chubb <chubb@laptop-HRP37O3Q>
2022-03-01 15:41:57 -0500

    created form submit javascrpit

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,10 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
-var taskToDoEl= document.querySelector("#task-to-do");
 
-var createTaskHandler = function(){
+var taskToDoEl= document.querySelector("#task-to-do");
+var formEl = document.querySelector("#task-form");
+
+var createTaskHandler = function(event){
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is ia new task.";
     taskToDoEl.appendChild(listItemEl);
 }
-buttonEl.addEventListener("click" , createTaskHandler); 
+
+
+formEl.addEventListener("submit", createTaskHandler);
